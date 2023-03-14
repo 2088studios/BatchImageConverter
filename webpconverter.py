3332929c1953d21dae5db57f163e76622d59e3ae
Isaiah Lindley <121307156+2088studios@users.noreply.github.com>
2023-03-13 20:25:22 -0500

    Update webpconverter.py
    Fully operational webp converter.

--- webpconverter.py
+++ webpconverter.py
@@ -1,18 +1,26 @@
 import os
+import glob
 from PIL import Image
+from pathlib import Path
 
-fileCount = 0;
+fileCount = 0
 
-for file in os.listdir("/home/ilindley/Downloads"):
-    if (file.endswith(".webp")):
-        #base, ext = os.path.splitext(file)
-        #os.replace(ext, '.png')
-        #file = base + '.png'
-        #file.replace('.webp', '.png')
-        
-        fileCount += 1
+# Gets users' downloads folder path
+path = Path.home() / "Downloads"
 
+# Gathers all webp files in
+image_list = path.glob("*.webp")
 
+for file in image_list:
+
+    fileName, ext = os.path.splitext(file)
+    outFile = fileName + ".png"
+    image = Image.open(file)
+    image.save(outFile)
+    os.remove(file)  # Deletes .webp file
+    fileCount += 1
+
+# Print messages
 if fileCount > 0:
     print("Script Complete!")
     print("# of files changed:", fileCount)
