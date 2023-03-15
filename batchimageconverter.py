63ada72fa33dcbba388c22ad4d74a9fa21ceadb3
Isaiah Lindley <2088studios@gmail.com>
2023-03-14 19:52:55 -0500

    Final comments and file name changes

--- batchimageconverter.py
+++ batchimageconverter.py
@@ -6,18 +6,18 @@ from pathlib import Path
 fileCount = 0
 
 # Gets users' downloads folder path
-path = Path.home() / "Downloads"
+path = Path.home() / "Downloads" #DEFAULT PATH
 
-# Gathers all webp files in
-image_list = path.glob("*.webp")
+# Gathers all files
+image_list = path.glob("*.webp") #DEFAULT CONVERSION
 
 for file in image_list:
 
     fileName, ext = os.path.splitext(file)
-    outFile = fileName + ".png"
+    outFile = fileName + ".png" #DEFAULT CONVERSION
     image = Image.open(file)
     image.save(outFile)
-    os.remove(file)  # Deletes .webp file
+    os.remove(file)  # Deletes old file
     fileCount += 1
 
 # Print messages
--- readme.md
+++ readme.md
@@ -1,15 +1,18 @@
-**WEBP File Converter**
+**Batch Image File Converter**
 
-This program converts all .WEBP files into .pngs in a single folder
+This program converts all image files in the desired directory
+from one format to another
 
+The default conversion is .webp to .png
 The default search folder is the users' /Downloads folder
 
 **HOW TO USE/INSTALL**
 
 1. (Optional) Change default path to desired path
-2. Run script
-3. Proft!
+2. (Optional) Change default file converstion
+3. Run script
+4. Proft!
 
 **Enjoy! :)**
 
-_Last updated ~ 3/13/2023_
+_Last updated ~ 3/14/2023_
